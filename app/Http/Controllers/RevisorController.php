41bb4f5cb547bea9c19a5c829a7fd3268c6948ca
JackKlik <bugibba94@gmail.it>
2023-07-03 19:45:12 +0200

    ciao

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\Controller;
 
 class RevisorController extends Controller
 {
